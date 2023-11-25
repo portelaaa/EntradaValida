4a64711c87c7be4b43d6ce3632a04410101ad60c
portelaaa <e107ti@etec.sp.gov.br>
2023-11-24 21:57:02 -0300

    Exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Entrada Válida");
+
+int numeroum = -1;
+
+while (numeroum < 0 || numeroum > 9)
+{
+    Console.WriteLine("Digite um número entre 1 e 9 (ou 0 para cancelar)...");
+    Int32.TryParse(Console.ReadLine(), out numeroum);
+}
+
+
+if (numeroum == 0)
+{
+    Console.WriteLine("Operação cancelada.");
+
+}
+else
+{
+    Console.WriteLine ($"Número selecionado = {numeroum}");
+}
+
+Console.WriteLine("Obrigado, volte sempre!");
